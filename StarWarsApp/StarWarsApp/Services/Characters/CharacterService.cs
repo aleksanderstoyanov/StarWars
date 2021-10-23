a75155b1f2f206d96503186c0ca5940a0355706f
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-10-23 18:16:57 +0300

    Adding initial service layer where business logic is being held.

---
+++ StarWarsApp/StarWarsApp/Services/Characters/CharacterService.cs
@@ -0,0 +1,28 @@
+﻿namespace StarWars.Services.Characters
+{
+    using StarWars.Data;
+    using StarWars.Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CharacterService : ICharacterService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CharacterService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IEnumerable<Character> GetAll()
+        {
+            return this.dbContext.Characters.ToList();
+        }
+
+        public IEnumerable<Character> GetTop3()
+        {
+            return this.dbContext.Characters.Take(3).ToList();
+        }
+    }
+}
---
+++ StarWarsApp/StarWarsApp/Services/Characters/ICharacterService.cs
@@ -0,0 +1,14 @@
+﻿namespace StarWars.Services.Characters
+{
+    using StarWars.Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface ICharacterService
+    {
+        IEnumerable<Character> GetAll();
+        IEnumerable<Character> GetTop3();
+    }
+}
