b2718bbe1c9c37f3a0c5a7b0bbd79f4b2581d6d9
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-10-23 22:14:22 +0300

    Adding new class library with static global constants whilst adding additional view model
    Added additional constants access through class library mostly revolving around validations. Also, adding additional view model for further create logic regarding characters

---
+++ StarWarsApp/StarWarsApp.Common/GlobalConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace StarWarsApp.Common
+{
+    public static class GlobalConstants
+    {
+        public static class Characters
+        {
+            public const int MinHeight = 1;
+            public const int MaxHeight = 500;
+
+            public const int MinMass = 1;
+            public const  int MaxMass = 1;
+
+            public const int MinNameLength = 2;
+            public const int MaxNameLength = 50;
+
+            public const int MinHairColorLength = 3;
+            public const int MaxHairColorLength = 40;
+
+            public const int MinSkinColorLength = 3;
+            public const int MaxSkinColorLength = 20;
+
+            public const int MinEyeColor = 3;
+            public const int MaxEyeColor = 20;
+
+            public const int MinGenderLength = 2;
+            public const int MaxGenderLength = 20;
+        }
+    }
+}
---
+++ StarWarsApp/StarWarsApp.ViewModels/Models/Characters/CharacterInputModel.cs
@@ -0,0 +1,54 @@
+﻿namespace StarWarsApp.ViewModels.Models.Characters
+{
+    using System.ComponentModel.DataAnnotations;
+    using static StarWarsApp.Common.GlobalConstants.Characters;
+    public class CharacterInputModel
+    {
+        [Required]
+        [StringLength(
+         MaxNameLength,
+         MinimumLength = MinNameLength,
+         ErrorMessage = "Name should be between 2 and 50 !")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(MinHeight, MaxHeight)]
+        public int Height { get; set; }
+
+        [Required]
+        [Range(MinMass, MaxMass)]
+        public int Mass { get; set; }
+
+        [Required]
+        [StringLength(
+         MaxHairColorLength,
+         MinimumLength = MinHairColorLength,
+         ErrorMessage = "Hair Color should be between 3 and 40 !")]
+        public string HairColor { get; set; }
+
+        [Required]
+        [StringLength(
+         MaxSkinColorLength,
+         MinimumLength = MinSkinColorLength,
+         ErrorMessage = "Skin Color should be between 3 and 20 !")]
+        public string SkinColor { get; set; }
+
+        [Required]
+        [StringLength(
+         MaxEyeColor,
+         MinimumLength = MinEyeColor,
+         ErrorMessage = "Eye Color should be between 3 and 20 !")]
+        public string EyeColor { get; set; }
+
+        [Required]
+        [StringLength(
+         MaxGenderLength,
+         MinimumLength = MinGenderLength,
+         ErrorMessage = "Gender should be between 2 and 20 !")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Url(ErrorMessage = "Image should be a valid image url !")]
+        public string Image { get; set; }
+    }
+}
