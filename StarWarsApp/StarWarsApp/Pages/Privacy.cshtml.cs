a479bfa55ce4d143e7f71d446c23c9d4989bc7ed
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-11-21 00:27:20 +0200

    Moving namespaces in order to keep convention

--- StarWarsApp/StarWarsApp/Pages/Privacy.cshtml.cs
+++ StarWarsApp/StarWarsApp/Pages/Privacy.cshtml.cs
@@ -1,13 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace StarWarsApp.Pages
+﻿namespace StarWarsApp.Pages
 {
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
