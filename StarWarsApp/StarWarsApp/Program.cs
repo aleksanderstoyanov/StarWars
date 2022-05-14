a262b9d3290932e0dea05801c84ec6594542d4b2
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2022-05-14 18:37:28 +0300

    Unify Program.cs and Startup.cs files

--- StarWarsApp/StarWarsApp/Program.cs
+++ StarWarsApp/StarWarsApp/Program.cs
@@ -1,19 +1,71 @@
-namespace StarWarsApp
-{
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Hosting;
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StarWarsApp.Data.Data;
+using StarWarsApp.Data.Data.Seeders;
+using StarWarsApp.Services.Services.Cache;
+using StarWarsApp.Services.Services.CharacterMovies;
+using StarWarsApp.Services.Services.Characters;
+using StarWarsApp.Services.Services.Contracts;
+using StarWarsApp.Services.Services.Movies;
+using StarWarsApp.Services.Services.Vehicles;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddTransient<ICharacterService, CharacterService>();
+builder.Services.AddTransient<IMoviesService, MovieService>();
+builder.Services.AddTransient<IVehicleService, VehicleService>();
+builder.Services.AddTransient<ICharacterMovieService, CharacterMovieService>();
+builder.Services.AddTransient<ICacheService, CacheService>();
+
+var app = builder.Build();
+var rootPath = builder.Environment.WebRootPath;
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+    new MoviesSeeder().SeedAsync(dbContext, rootPath + @"\" + "movies.json").GetAwaiter().GetResult();
+    new CharactersSeeder().SeedAsync(dbContext, rootPath + @"\" + "characters.json").GetAwaiter().GetResult();
+    new VehiclesSeeder().SeedAsync(dbContext, rootPath + @"\" + "vehicles.json").GetAwaiter().GetResult();
 }
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+});
+
+app.MapRazorPages();
+
+app.Run();
