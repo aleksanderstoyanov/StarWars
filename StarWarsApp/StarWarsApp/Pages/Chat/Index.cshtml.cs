c5151563bed9912a4723ebe2ceaa27a189c1293e
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2022-05-15 23:58:55 +0300

    add client hub connection

--- StarWarsApp/StarWarsApp/Pages/Chat/Index.cshtml.cs
+++ StarWarsApp/StarWarsApp/Pages/Chat/Index.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace StarWarsApp.Pages.Chat
 {
+    [Authorize]
     public class ChatModel : PageModel
     {
         public void OnGet()
